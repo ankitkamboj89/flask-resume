ae5c5daa9e9821d571cf6c3906ccc1dcd75b512f
Ankit Kamboj <ankitkam@umich.edu>
2025-08-03 15:51:31 -0400

    Initial commit of Flask resume website

---
+++ cv.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def executive_summary():
+    return render_template('executive_summary.html')
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+@app.route('/technical-knowledge')
+def technical_knowledge():
+    return render_template('technical_knowledge.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
